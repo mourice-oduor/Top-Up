61ca298bb99acbda814a14309afe04d0347edef0
mourice-oduor <otienomaurice364@gmail.com>
2022-08-05 14:31:25 +0300

    update frontend

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Top-Up Mama</Col>
+          <Col className='text-center py-3'>Copyright &copy2022; Top-Up Mama</Col>
         </Row>
       </Container>
     </footer>
