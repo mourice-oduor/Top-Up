561f70dbcc965e384e85c2673946ab1577d7f225
mourice-oduor <otienomaurice364@gmail.com>
2022-08-05 14:32:03 +0300

    update frontend

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
 import { useAppSelector } from "../redux/hooks/hooks";
+import { RootState } from '../redux/store';
 
-
-const userLogin = useAppSelector((state) => state.user);
+const userLogin = useAppSelector((state: RootState) => state.user );
 const { userInfo }  = userLogin
 
 const firstName = userInfo ? userInfo?.first_name : null
 
 function Home() {
   return firstName ? (
-    <h1>Welcome {firstName}!</h1>
+    <h1>Welcome {firstName}</h1>
   ) : (
-    <h2>Welcome to the Home Page!</h2>
+    <h1>Welcome to the Home Page!</h1>
   )
+  // return(
+  //   <div>
+  //     <h1>Welcome to the Home Page!</h1>
+  //   </div>
+  // )
 }
 
 export default Home
