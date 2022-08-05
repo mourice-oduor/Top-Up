90279e503d70fbb924cbd42c052a11a5470fe336
mourice-oduor <otienomaurice364@gmail.com>
2022-08-05 14:32:16 +0300

    update frontend

--- client/src/redux/actions/userActions.tsx
+++ client/src/redux/actions/userActions.tsx
@@ -17,11 +17,11 @@ export const getAllUsers =
   () => async (dispatch: Dispatch<any> ) => {
     dispatch({
       type: "LOADING",
-      payload: true,
+      payload: true
     });
 
     try {
-      const response = await axios.get("https://reqres.in/api/users?page=2");
+      const response = await axios.get("https://reqres.in/api/users?page=1");
       dispatch({
         type: "GET_ALL_USERS",
         payload: response.data,
@@ -39,7 +39,7 @@ export const getAllUsers =
     }
   };
 
-export const register =( first_name: string, last_name: string, email: string, password: string ) =>
+export const register =( email: string, password: string ) =>
   async (dispatch: Dispatch<any> )=> {
     try {
       dispatch({ type: REGISTER_USER_REQUEST });
@@ -51,7 +51,7 @@ export const register =( first_name: string, last_name: string, email: string, p
       };
 
       const { data } = await axios.post(
-        "https://reqres.in/api/register", { first_name, last_name, email, password }, config
+        "https://reqres.in/api/register", { email, password }, config
       );
 
       dispatch({ type: REGISTER_USER_SUCCESS, payload: data });
@@ -63,7 +63,7 @@ export const register =( first_name: string, last_name: string, email: string, p
         window.location.href = "/login";
       }, 200);
 
-      dispatch({ type: LOGIN_SUCCESS, payload: data });
+      dispatch({ type: LOGIN_REQUEST, payload: data });
 
       localStorage.setItem("userInfo", JSON.stringify(data));
     } catch (error: any) {
@@ -91,11 +91,14 @@ export const login = (email: string, password: string) => async (dispatch: Dispa
       "https://reqres.in/api/login", { email, password }, config
     );
 
+    setTimeout(() => {
+      window.location.href = "/home";
+    }, 200);
+
     dispatch({ type: LOGIN_SUCCESS, payload: data });
     <Alert variant="success">
       <Alert.Heading>Login successfull</Alert.Heading>
     </Alert>;
-    // message.success('Login success');
 
     localStorage.setItem("userInfo", JSON.stringify(data));
 
@@ -111,10 +114,12 @@ export const login = (email: string, password: string) => async (dispatch: Dispa
 };
 
 export const logout = () => async (dispatch: Dispatch<any> ) => {
-  // message.success('Successfully Logged you out');
   <Alert variant="success">
     <Alert.Heading>Successfully Logged you out</Alert.Heading>
   </Alert>;
+  setTimeout(() => {
+    window.location.href = "/home";
+  }, 200);
 
   dispatch({ type: LOGOUT_SUCCESS });
   localStorage.removeItem("userInfo");
