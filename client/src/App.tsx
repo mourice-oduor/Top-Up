aee6f3a90d5178da64f324684086b8b56331845f
mourice-oduor <otienomaurice364@gmail.com>
2022-08-06 18:06:34 +0300

    update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/Header";
@@ -11,26 +10,11 @@ import Home from "./pages/Home";
 import Profile from "./pages/profile/Profile";
 import UpdateProfile from "./pages/profile/UpdateProfile";
 import Users from "./pages/Users";
-// import { string } from "yup";
-// import { logout } from "./redux/actions/userActions";
-// import { useAppDispatch } from "./redux/hooks/hooks";
+// import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  // const [firstName, setFirstName] = useState("");
-  // const dispatch = useAppDispatch();
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await fetch(`https://reqres.in/api/users/${id}`, {
-  //       headers: { "Content-Type": "application/json" },
-  //       credentials: "include",
-  //       mode: "cors",
-  //     });
-
-  //     const data = await response.json();
-  //     setFirstName(data?.first_name);
-  //   })();
-  // });
+  // @ts-ignore
+  const user = JSON.parse(localStorage.getItem("userInfo"));
 
   return (
     <BrowserRouter>
@@ -38,12 +22,13 @@ function App() {
       <main>
         <Container>
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={ <Profile />} />
-            <Route path="/profile/edit" element={<UpdateProfile />} />
-            <Route path="/users" element={<Users />} />
+            <Route path='/' element={ <PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/profile/edit" element={<PrivateRoute><UpdateProfile /></PrivateRoute>} />
+            <Route path="/users" element={<PrivateRoute><Users /></PrivateRoute>} />
           </Routes>
         </Container>
       </main>
@@ -52,3 +37,13 @@ function App() {
   );
 }
 export default App;
+
+export function PrivateRoute({children}: any ) {
+  if(localStorage.getItem('userInfo'))
+  {
+      return children;
+  }
+  else{
+      return <Navigate to='/register' replace={true} />;
+  }
+}
