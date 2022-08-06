5bafe4f6267c6825645f20bb8e7944ededf3810e
mourice-oduor <otienomaurice364@gmail.com>
2022-08-06 15:54:28 +0300

    update styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,47 +2,52 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Profile from "./components/Profile";
-import Users from "./components/Users";
+import Profile from "./pages/profile/Profile";
+import UpdateProfile from "./pages/profile/UpdateProfile";
+import Users from "./pages/Users";
+// import { string } from "yup";
+// import { logout } from "./redux/actions/userActions";
+// import { useAppDispatch } from "./redux/hooks/hooks";
 
 function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const dispatch = useAppDispatch();
 
-  const [firstName, setFirstName] = useState('')
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch(`https://reqres.in/api/users/${id}`, {
+  //       headers: { "Content-Type": "application/json" },
+  //       credentials: "include",
+  //       mode: "cors",
+  //     });
 
-  useEffect(() => {
-    ;(async () => {
-      const response = await fetch('https://reqres.in/api/users/3', {
-        headers: { 'Content-Type': 'application/json' },
-        credentials: 'include',
-        mode: 'cors'
-      })
-      
-      const data = await response.json()
-      setFirstName(data.first_name)
-    })()
-  })
+  //     const data = await response.json();
+  //     setFirstName(data?.first_name);
+  //   })();
+  // });
 
   return (
     <BrowserRouter>
-      <Header firstName={firstName} setFirstName={setFirstName} />
+      <Header />
       <main>
         <Container>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={ <Profile />} />
+            <Route path="/profile/edit" element={<UpdateProfile />} />
             <Route path="/users" element={<Users />} />
           </Routes>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
