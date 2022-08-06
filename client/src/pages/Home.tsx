1e6c8fb0f34b6b809efb7485fbec729351ae290d
mourice-oduor <otienomaurice364@gmail.com>
2022-08-06 18:07:26 +0300

    update page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,22 @@
 import { useAppSelector } from "../redux/hooks/hooks";
 import { RootState } from '../redux/store';
 
-const userLogin = useAppSelector((state: RootState) => state.user );
-const { userInfo }  = userLogin
+// const userLogin = useAppSelector((state: RootState) => state.user );
+// const { userInfo }  = userLogin
 
-const firstName = userInfo ? userInfo?.first_name : null
+// const firstName = userInfo ? userInfo?.first_name : null
 
 function Home() {
-  return firstName ? (
-    <h1>Welcome {firstName}</h1>
-  ) : (
-    <h1>Welcome to the Home Page!</h1>
-  )
-  // return(
-  //   <div>
-  //     <h1>Welcome to the Home Page!</h1>
-  //   </div>
+  // return firstName ? (
+  //   <h1>Welcome {firstName}</h1>
+  // ) : (
+  //   <h1>Welcome to the Home Page!</h1>
   // )
+  return(
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+    </div>
+  )
 }
 
 // export default Home
