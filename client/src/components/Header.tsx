b47a946b30d9472e53bee42f7c0442cfa9d46ae2
mourice-oduor <otienomaurice364@gmail.com>
2022-08-06 15:54:41 +0300

    update header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,52 +1,54 @@
-import { SyntheticEvent } from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { SyntheticEvent } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { useAppDispatch } from "../redux/hooks/hooks";
 import { logout } from "../redux/actions/userActions";
 
 interface Props {
   firstName: string;
   setFirstName: (firstName: string) => void;
-};
+}
 
-function Header({ firstName, setFirstName }: Props) {
+// function Header({ firstName, setFirstName }: Props) {
+function Header() {
 
   const dispatch = useAppDispatch();
 
   const logoutHandler = async (e: SyntheticEvent) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    dispatch(logout())
-    setFirstName('')
-  }
+    dispatch(logout());
+    // setFirstName("");
+  };
 
   return (
-    <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect sticky='top'>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect sticky="top">
       <Container>
-        <Navbar.Brand href='/'>Top-Up Mama</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className="me-auto">
+        <Navbar.Brand href="/">Top-Up Mama</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
             <Nav.Link href="/users">Users</Nav.Link>
-            <Nav.Link href='/profile'>Profile</Nav.Link>
+            <Nav.Link href="/profile">Profile</Nav.Link>
           </Nav>
 
           <Nav>
-            {firstName ? (
-              <Nav className='ms-auto'>
+            {{} ? (
+              <Nav className="ms-auto">
                 <Nav.Link onClick={logoutHandler}>Logout</Nav.Link>
               </Nav>
             ) : (
-              <Nav className='ms-auto'>
-                <Nav.Link href='/register'>Register</Nav.Link>
-                <Nav.Link href='/login'>Login</Nav.Link>
+              <Nav className="ms-auto">
+                <Nav.Link href="/register">Register</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
               </Nav>
             )}
           </Nav>
-
         </Navbar.Collapse>
+        
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
